8536a227cff738cd416cfebca1413d953db61d82
pdewan <dewan@cs.unc.edu>
2015-08-02 03:05:05 -0400

    Uncommented point pattern

--- src/lectures/graphics/Point.java
+++ src/lectures/graphics/Point.java
@@ -1,6 +1,8 @@
 package lectures.graphics;
 import util.annotations.Explanation;
-//@StructurePattern(StructurePatternNames.POINT_PATTERN)
+import util.annotations.StructurePattern;
+import util.annotations.StructurePatternNames;
+@StructurePattern(StructurePatternNames.POINT_PATTERN)
 @Explanation("Location in Java coordinate System.")
 public interface Point {
 	public int getX(); 
