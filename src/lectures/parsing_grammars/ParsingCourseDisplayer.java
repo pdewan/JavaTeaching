cfb3e4e86308912292538263308f29c7889add70
pdewan <dewan@cs.unc.edu>
2016-07-30 04:32:14 -0400

    Grammar

--- src/lectures/parsing_grammars/ParsingCourseDisplayer.java
+++ src/lectures/parsing_grammars/ParsingCourseDisplayer.java
@@ -29,9 +29,12 @@ import lectures.inheritance.abstract_classes.CourseDisplayer;
 
 // Do you see the pattern for inputting freshman seminars and course numbers?
 // Can you describe in English this pattern?
+// Each sequence of legal input lines is called a sentence. These are not
+// necessarily legal English sentences, they are simply sentences described
+// by out pattern for input.
 
 // Such a pattern can be described formally by the following grammar:
-//<CourseList> -> <Course> *.
+//<CourseList> -> <Course> *. 
 //<Course> -> <RC> | <FS>
 //<RC> -> RC <Title> <Dept> <Number>
 //<FS> -> FS <Title> <Dept>
@@ -39,27 +42,29 @@ import lectures.inheritance.abstract_classes.CourseDisplayer;
 //<Dept> -> String
 //<Number> -> Integer
 
-// The grammear consists of a series of production rules.
+// The grammar consists of a series of production rules which define or derive legal sentences.
 // This grammar says that the input lines consist of a <CourseList>
-// A <CourseList>, in turn consists of an unlimited number of <Course> input lines followed by a period.
-// * means repetition.
+// A <CourseList>, in turn consists of or derives an unlimited number of <Course> input lines followed by a period.
+// * stands for repetition.
 // A <Course> can either be a <RC> (regular course) or <FS> (Freshman seminar)
-// | means alernation
-// An <RC> is followed by the terminal or literal RC followed by a <Title> <Dept> <Number>
-// <Title> and <Dept> are string values.
-// <Number> is an arbitrary integer.
+// | stands for alternation.
+// An <RC> is followed by the the literal "RC" followed by a <Title> <Dept> <Number>
+// <Title> and <Dept> are string values. <Number> is an arbitrary integer.
 // A terminal or literal does not appear on the left hand side (LHS) of a production and thus
 // does not need elaboration. (We know what strings and integers are). It is not enclosed in angle brackets.
-// A non terminal must appear on the LHS and can appear in the right
+// A non terminal must appear on the LHS of at least one production and can appear in the right
 // hand side also. It is enclosed in angle brackets. The root non terminal appears only on the LHS.
 
-// A grammar provides us with a structured way to parse input, that is, convert scanned tokens into
-// into a form called the parse tree that can be processed by the program.
+
+// A grammar provides us with a structured way to write a parser.
+// A parser converts scanned tokens in a legal sentence into
+// into a form called the parse tree, which can the be further processed by the program.
 // In this program, we are trying to convert the input into a course list, which is the tree that
-// will be processed.
+// will be processed. Each token is assumed to appear on a separate line to make our scanning easy.
 
 // This structured way is called recursive descent parsing in which a separate
-// parsing method is created for each non terminal.
+// parsing method is created for each non terminal that can recursively
+// call parsing methods for non terminals defining or deriving smaller parts of the sentence.
 
 
 
