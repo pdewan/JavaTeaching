786b609a3697aea60556faf6118187e21fcb3dfb
pdewan <dewan@cs.unc.edu>
2016-08-24 10:55:14 -0400

    Praxis scanning

--- src/lectures/scanning/AConsoleReadingUpperCasePrinter.java
+++ src/lectures/scanning/AConsoleReadingUpperCasePrinter.java
@@ -1,47 +1,75 @@
 
 /**
+ * RUNNING A PROGRAM
+ * This is an example of a Java program called a class in Java.
+ * There are many ways to execute a class,
+ * If you are editing the class and your insertion pont is in it,
+ * Right click->Debug As->Java Application is one way.
+ * Use it or some other way you know to run this program.
+ * 
  * PACKAGES
- * A class may be in a named hierarchical package much as a file can be in a named hierarchical folder
- * this class is in a package called scanning which is a supackage of lectures
- * a package name should start with a lowe case letter
+ * A class (named AConsoleReadingUpperCasePrinter below)  may be contained in a named hierarchical package much as a file can be in a named hierarchical folder
+ * This class is in a package called scanning which is a sub-package of lectures.
+ * In fact, for each package, the OS creates a folder and for each class in a package it creates a file in the 
+ * folder of its package.
+ * This correspondence means that two different packages can 
+ * contain classes with the same short name. However a package cannot 
+ * contain two classes with the same name.
+ * A package name should start with a lower case letter.
+ * 
+ * FULL VS SHORT NAME
+ * The package name followed by . followed by the short name of the class is its full or long name.
+ * The full name of this class is lectures.scanning.AConsoleReadingUpperCasePrinter
+ * 
  */
 package lectures.scanning; 
-	/**
+	/**	  
+	 * 
 	 * IMPORTS
-	 * Below, we are importing an external class not in the package of this class
-	 * The last part of the import identify the short name of the class and the preceding parts the package of the class.
-	 * the package name followed by . followed by the short name of the class is its full or long name
-	 * What is the short name of the class declared below by us?
-	 * What is the full name of this class?
-	 * What if you comment this import out?
+	 * 
+	 * If a class C1 refers a class C2 in a different package, then it must use either the full name
+	 * of class C2 or import C2.
+	 * 
+	 * Below, we are importing an external class not in the package of this class.
+	 * What happens if you comment this import out?
 	 */
 import java.util.Scanner; 
 	/**
 	 * CLASSS
-	 * Every program must have at least one class that declares a main method.
+	 * Every runnable program must have at least one class that declares a main method.
 	 * A class is like a file in non object oriented languages.
 	 * A class name should be camel case starting with an upper case letter.
+	 * That is, the first word in the name should be a lowercase letter
+	 * and  each subsequent word should start with an uppercase letter.
 	 */
 public class AConsoleReadingUpperCasePrinter { 
 	
 	/**
 	 * MAIN METHOD HEADER
-	 * Syntax of main method shown below.
+	 * Syntax of main method shown below. This is the first method called when
+	 * the program is run, and can of course call othe methods.
 	 * Methods correspond to procedures and functions in other languages.
-	 * Method names should be camel case starting with lowercase letter.
+	 * A method name should be camelCase 
 	 * Everything before the first curly brace is the method header.
 	 */
 	public static void main(String[] args) 
 	/*
-	 * What happens if you use the following header instead, can you execute the program?
+	 * What happens if you use each of the following headers for main instead, can you execute the program?
 	 * Comment out the header above and uncomment the following to see what happens?
-	 * What is the difference between the two headers?
+	 * You can comment and uncomment a set of selected lines using the shortcut CTRL/
+	 * In the case of a single line, it is sufficient to have the insertion point in the line
 	 */
 //	public static void main(String args) {
+//	public static void main(String[] anArgs) {
+//	public static void main(String anArgs[]) {
+//	public void main(String anArgs[]) {
+//	public static int main(String anArgs[]) {
+
+
 	/**
 	* METHOD BODY
 	* The code between the outermost curly braces is the method body.
-	* A method body is a sequene of statements teminated with semi colons;
+	* A method body is a sequence of statements terminated with semi colons;
 	*/
 	{
 
@@ -54,7 +82,7 @@ public class AConsoleReadingUpperCasePrinter {
 		 * WHILE LOOPS
 		 * Syntax of Java while loop shown below
 		 * The loop "scans" scannedString, that is, it examines every character in the string and finds
-		 * substrings of interest called tokens
+		 * substrings of interest called tokens.
 		 * How many characters in each token identified by this loop?
 		 */
 		while (index < scannedString.length()) {
--- src/lectures/scanning/package-info.java
+++ src/lectures/scanning/package-info.java
@@ -1,6 +1,46 @@
-/*
+/**
  * Code associated with the scanning teaching unit in: http://www.cs.unc.edu/~dewan/comp401/current/
  * The word and PPT documents created for the unit describe the purpose and motivation for this unit in
- * great depth
+ * great depth.
+ * 
+ * Visit the following classes for this praxis in this order:
+ * {@link lectures.scanning.AConsoleReadingUpperCasePrinter}
+ * Click on the name of the referenced class name and Press F3 (maybe Fn F3 or CMD F3)
+ * to automatically navigate to it.
+ * 
+ * After performing the praxis, answer the following questions in draft form here.
+ * Once you are sure of your answers, take the Scanning Sakai quiz.
+ * 
+ * Packages
+ * 
+ * T/F For each class, a folder is created.
+ * T/F For each class, a file is created.
+ * T/F For each package, a folder is created.
+ * T/F For each package, a folder is created.
+ * 
+ * T/F The full name of a class contains its package and class name.
+ * T/F The short name of a class is its package name.
+ * 
+ * T/F It makes sense for a class to import classes in its package.
+ * T/F It makes sense for a class to import classes not in its package.
+ * 
+ * Executable classes
+ *
+ * T/F Any class can be executed.
+ * T/F Only a class with a main method can be executed.
+ * 
+ * 
+ * T/F A main method must have an argument named arg.
+ * T/F A main method can have a String argument.
+ * T/F Constraints put on the main method makes it easier for a human being to find it in the containing main class
+ * T/F Constraints put on the main method makes it easier for the Java interpreter, which executes a main method, to find it in the containing class.
+ * T/F Constraints put on the main method can prevent programming errors such as returning a value.
+ * 
+ * Scanning
+ * 
+ * A token produced by scanning a string s is a substring of s.
+ * A token produced by scanning a string s can have non consecutive characters of s.
+ * All tokens produced by scanning a string must have the same length.
+ *
  */
 package lectures.scanning;
