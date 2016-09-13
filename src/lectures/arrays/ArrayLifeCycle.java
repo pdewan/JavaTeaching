f8b7f9e6f4ed30138fd56e4e06ff27fa6806c640
pdewan <dewan@cs.unc.edu>
2016-09-13 16:57:05 -0400

    Arrays

--- src/lectures/arrays/ArrayLifeCycle.java
+++ src/lectures/arrays/ArrayLifeCycle.java
@@ -1,51 +1,118 @@
 package lectures.arrays;
-// A Library class for printing arrays
+/*
+  The following is a library class we use for printing array
+ */
 import java.util.Arrays;
 
 import lectures.interfaces.ALoopingFactorialSpreadsheet;
 import lectures.interfaces.ARecursiveFactorialSpreadsheet;
 import lectures.interfaces.FactorialSpreadsheet;
 
+/*
+
+ * This class should help you understand the different ways of declaring 
+ * array variables, creating array objects, and assigning elements of 
+ * array objects, which are distinct steps.
+ *  
+ * 
+ */
 public class ArrayLifeCycle {
-	static FactorialSpreadsheet[] uninitializedArray; 
+	
+	/*
+	 *  The element type of an array 
+	 * 	   (a) must be a primitive type.
+	 *     (b) must be an object type.
+	 *     (c) can be a primitive or object type.
+	 *   
+	 * An array is a sequence of  variables, called elements of the array,
+	 * all of which have to be of a certain type, called the element type of the 
+	 * array.
+	 * 
+	 * The element types in the arrays below are int and FactorialSpreadsheet.
+	 * 
+	 */
+	
+	static int[] uninitializedPrimitiveArray;
+	static int[] initializedPrimitiveArray = {5, 10};
+	static int[] initializedEmptyArray = {};
+
+	 
+	static FactorialSpreadsheet[] uninitializedObjectArray; 
 	static FactorialSpreadsheet[] uninitializedElements = new FactorialSpreadsheet[5];
 	static FactorialSpreadsheet[] initializedElements = {
 			new ALoopingFactorialSpreadsheet(),
-			new ARecursiveFactorialSpreadsheet()
-			
+			new ARecursiveFactorialSpreadsheet()			
 	};
 
 	public static void main (String[] anArgs) {
+		/*
+		 * 
+		 * (T/F) It is possible for an array variable to have
+		 * 		 the null value.
+		 * (T/F) It is possible for an array to have no elements.
+		 * (T/F) It is possible for an array object to have null elements.
+		 */
+		System.out.println("Uninitialized primitive array:" +
+				Arrays.toString(uninitializedPrimitiveArray));
+		System.out.println("Uniitialized object array:" +
+				Arrays.toString(uninitializedObjectArray));		
+		System.out.println("Uninitialized primitive array:" +
+				Arrays.toString(uninitializedElements));
+		System.out.println("Initialized elements:" +
+				Arrays.toString(initializedElements));
 		
-		System.out.println(Arrays.toString(uninitializedArray));
-		System.out.println(Arrays.toString(uninitializedElements));
-		System.out.println(Arrays.toString(initializedElements));
-		
-		System.out.println (uninitializedElements.length);
-		// Put break point here	
+		System.out.println ("Length of uninitializedElements: +"
+				+ uninitializedElements.length);
+		// Put break point below	
 		uninitializedElements[0] = new ARecursiveFactorialSpreadsheet();
 		System.out.println (uninitializedElements.length);
-		System.out.println(Arrays.toString(uninitializedElements));
+		System.out.println("Length of uniinitializedElements:" +
+				Arrays.toString(uninitializedElements));
 
 		uninitializedElements = new FactorialSpreadsheet[] {
 				new ALoopingFactorialSpreadsheet()
 		};
-		System.out.println (uninitializedElements.length);
+		System.out.println ("Length of uniinitializedElements:" +
+				uninitializedElements.length);
 		System.out.println(Arrays.toString(uninitializedElements));
-		// Did the array assigned to uninitializedElements change size ever in this program?
+		
+		/*
+		 * (T/F) The size of an array can change dynamically during program 
+		 * execution.
+		 * 
+		 * 
+		 * Did the array assigned to uninitializedElements change size ever in 
+		 * this program?		
+		*/
 
-	}	
-	// Understand the different ways of declaring array variables, creating array objects,
-	// and assigning elements of array objects, which are distinct steps
-	// To better understand arrays, debug-run the program and look at the variable
-	// uninitialized elements. What Id (pointer) do you see next to the array variable name?
-	// What size (in brackets) do you see next to it. 
-	// bottom you will see all of it
-	// What are the values of elements and their ids?
-	// Now press F6, step over to step over the next statement.
-	// View the variable in the same way. Did the Id change? 
-	// Press F6 to step over until you hit the last println
-	// View the variable in the same way. Did the Id change? 
+	}
+	/*	
+	 * (T/F) The pointer or address of an array object changes each time an 
+	 * element of it changes.
+	 * 
+	 * (T/F) The size of an array object changes each an element of it changes.
+	 * 
+	 * (T/F) The pointer of an array element can change when an assignment is 
+	 * made to the element.
+	 * 
+	 * To better understand arrays, debug-run the program and look at the variable
+	 * uninitialized elements. 
+	 * 
+	 * What Id (pointer) do you see next to the array variable name?
+	 * 
+	 * What size (in brackets) do you see next to it? 
+
+	 * What are the values of elements and their ids?
+	 * 
+	 * Now press F6, step over to step over the next statement.
+	 * 
+	 * View the variable in the same way. Did the Id change? 
+	 * 
+	 * Press F6 to step over until you hit the last println
+	 * 
+	 * View the variable in the same way. Did the Id change? 
+	 * 
+	 */
 
 		
 	
---
+++ src/lectures/arrays/package-info.java
@@ -0,0 +1,40 @@
+/**
+ * PRAXIS INFO
+ * This is a refresher on arrays for those who have forgotten it and also brings
+ * out some subtleties in object arrays for whose who have not seen object arrays.
+ * For those who know arrays well, just answer the quiz questions.
+ * 
+ * Visit the class ArrayLifeCycle
+ * 
+ * SAKAI QUIZ
+ * 
+ *  The element type of an array 
+ * 	   (a) must be a primitive type.
+ *     (b) must be an object type.
+ *     (c) can be a primitive or object type.
+ *     
+ 
+ * (T/F) It is possible for an array variable to have
+ * 		 the null value.
+ * 
+ * (T/F) It is possible for an array to have no elements.
+ * 
+ * (T/F) It is possible for an array object to have null elements.
+ * 
+ * (T/F) The size of an array can change dynamically during program 
+		 execution.
+		 
+ * (T/F) The pointer or address of an array object changes each time an 
+ * element of it changes.
+ * 
+ * (T/F) The size of an array object changes each an element of it changes.
+ * 
+ * (T/F) The pointer of an array element can change when an assignment is 
+ * made to the element.
+ * 
+ * 
+ */
+package lectures.arrays;
+/*
+ * 
+ */
