80e7ec2ba1e60b354e69926e121005cffa0c27ab
pdewan <dewan@cs.unc.edu>
2016-09-20 20:15:37 -0400

    In the middle of Tree, DAG praxis

--- src/lectures/composite/tree_dag_graph_objects_windows/WindowTreeCreator.java
+++ src/lectures/composite/tree_dag_graph_objects_windows/WindowTreeCreator.java
@@ -5,20 +5,37 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
-// Let us create a logical and physical structure very different from the cartesian plane one
-// This structure composes different kinds of widgets - 
-// items that can displayed on the screen and possibly manipulated by the user
-// The widgets used here are a button, a textfield, two panels, a split pane, and a frame - top level window.
-// If we want a widget to be displayed in another widget, then we must make it a child of the latter.
-// Or conversely, a child of a widget is displayed within the latter.
-// Every displayed widget must have a frame ancestor, otherwise it is not displayed.
-// Study the program, and run it in the debug mode, stopping at the break point
+/**
+ * Let us create a logical and physical structure very different from the
+ * Cartesian Plane - one that teaches you something about how a GUI is 
+ * implemented.
+ * 
+ * This structure composes different kinds of widgets - 
+ * items that can displayed on the screen and possibly manipulated by the user.
+ * 
+ * 
+ * The widgets used here are a button, a textfield, two panels, a split pane, 
+ * and a frame - top level window.
+ * 
+ * If we want a widget to be displayed in another widget, 
+ * then we must make it a child of the latter.
+ * 
+ * Or conversely, a child of a widget is displayed within the area of the parent
+ * widget.
+ * 
+ * Every displayed widget must have a frame ancestor, otherwise it is not 
+ * displayed.
+ * 
+ * Study the program, and run it in the debug mode, stopping at the break point
+ * 
+ *
+ */
 public class WindowTreeCreator {
 	public static void main (String[] args) {
 		createTree();
 	}
 	public static JFrame createTree () {
-		JFrame frame = new JFrame(); // Creating widget structure root
+		JFrame frame = new JFrame(); // creating widget structure root
 		JSplitPane splitPane = new JSplitPane();
 		frame.add(splitPane); // split pane is now a descendant of frame
 		JPanel leftPanel = new JPanel();
@@ -35,9 +52,20 @@ public class WindowTreeCreator {
 		// 
 		return frame;
 	}
-	// When the program stops, verify the containment relationship on the screen among widgets and their children.
-	// Now verify the parent child relationship itself. Start with the splitPane. Each of these
-	// widgets has an instance variable called component, which can be expanded to see the children. 
-	// Verify that you can reach the text field or button from the splitPane.
+/*
+ * When the program stops, verify the containment relationship on the screen 
+ * among widgets and their children.
+ * 
+ * Now verify the parent child relationship itself. 
+ * 
+ * Start with the splitPane.
+ * 
+ *  Each of these
+ * widgets has an instance variable called component, which can be expanded 
+ * to see the (physical) children. 
+ * 
+ * Verify that you can reach the text field or button from the splitPane.
+ * 
+ */
 
 }
---
+++ src/lectures/composite/tree_dag_graph_objects_windows/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * PRAXIS_INFO
+ * 
+ * The notion of structured types allows us to define three important concepts
+ * in CS: trees, DAGs and Graphs. 
+ * 
+ * We will focus on trees in this course -creating a DAG or a graph will in 
+ * fact be an error. 
+ * 
+ * The reason for mentioning them is mainly to avoid such errors.
+ * 
+ * VISIT_INFO
+ * 
+ * WindowTreeCreator
+ * WindowDAGCreator
+ * WindowGraphCreator
+ * ADAGCartesianPlane
+ */
+
+package lectures.composite.tree_dag_graph_objects_windows;
