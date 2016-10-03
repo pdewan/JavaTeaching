2cacac7281132c63770bcc49f6e1f39781a0f319
pdewan <dewan@cs.unc.edu>
2016-10-03 18:51:20 -0400

    Java Collection hierrachy

--- src/lectures/arrays/collections_kinds/JavaCollectionHierarchy.java
+++ src/lectures/arrays/collections_kinds/JavaCollectionHierarchy.java
@@ -7,7 +7,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-// Java COLLECTION HIERARCHY
+/*
+ * Look at the code below
+ * 
+ * (T/F) An ArrayList is an instance of a Map.
+ * (T/F) An ArrayList is an instance of a Collection.
+ * (T/F) A HashSet is an  instance of a List.
+ * (T/F) A HashSet is an  instance of a Collection.
+ * (T/F) A HashMap is an instance of a Collection
+ */
 public class JavaCollectionHierarchy {
 	public static void instanceOf(Object anObject) {
 		System.out.println (anObject instanceof List);
--- src/lectures/arrays/collections_kinds/MapUser.java
+++ src/lectures/arrays/collections_kinds/MapUser.java
@@ -52,3 +52,6 @@ public class MapUser {
 	}
 	
 }
+/*
+ * Next class: JavaCollectionHierarchy
+ */
