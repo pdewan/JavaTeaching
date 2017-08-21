a2b49f0d3a8738dc1daf6d88fa9b447e877b9966
pdewan <dewan@cs.unc.edu>
2017-08-21 10:24:40 -0400

    Changed scanning praxis

--- src/lectures/scanning/AConsoleReadingUpperCasePrinter.java
+++ src/lectures/scanning/AConsoleReadingUpperCasePrinter.java
@@ -7,20 +7,34 @@
  * Right click->Debug As->Java Application is one way.
  * Use it or some other way you know to run this program.
  * 
- * If you do nto see the Java Application option in Debug As, or your get a console message
- * Cannot run or load class, then your project is misconfigured and you have not deleted the
+ * If you do not see the Java Application option in Debug As, or your get a 
+ * console message:
+ * "Cannot run or load class"
+ * then your project is misconfigured and you to delete the
  * oeall.jar file from the class path.
  * 
- * Look at the PPT on importing JavaTeaching again on how to do so. You have to select
- * the project then Right Click->Properties->Build Path->Libraries. Select oeall22.jar (it should
- * be red, and remove it. You will get a baunch of errors now but you can do the praxis. Look at
+ * Look at the ObjectEditor PPT
+ * http://www.cs.unc.edu/~dewan/comp401/current/Lectures/ObjectEditorLib.pdf
+ * on importing JavaTeaching  on how to do so. You have to select
+ * the project then Right Click->Properties->Build Path->Libraries. 
+ * Select oeall22.jar (it should be red, and remove it. 
+ * You will get a bunch of errors now but you can do the praxis. Look at
  * the objecteditor ppt on how to add your downloaded oeall to the class/build path.
  * 
  * PACKAGES
- * A class (named AConsoleReadingUpperCasePrinter below)  may be contained in a named hierarchical package much as a file can be in a named hierarchical folder
+ * How is a package stored in the file system?
+ * T/F For each class, a folder is created.
+ * T/F For each class, a file is created.
+ * 
+ * How is a class stored in the file system?
+ * T/F For each package, a folder is created.
+ * T/F For each package, a file is created.
+ * 
+ * A class (named AConsoleReadingUpperCasePrinter below) may be contained in 
+ * a named hierarchical package much as a file can be in a named hierarchical folder
  * This class is in a package called scanning which is a sub-package of lectures.
- * In fact, for each package, the OS creates a folder and for each class in a package it creates a file in the 
- * folder of its package.
+ * In fact, for each package, the OS creates a folder and for each class in a 
+ * package it creates a file in the folder of its package.
  * This correspondence means that two different packages can 
  * contain classes with the same short name. However a package cannot 
  * contain two classes with the same name.
@@ -29,8 +43,13 @@
  * Why are packages useful?
  * 
  * FULL VS SHORT NAME
- * The package name followed by . followed by the short name of the class is its full or long name.
- * The full name of this class is lectures.scanning.AConsoleReadingUpperCasePrinter (if you have made a copy
+ * T/F The full name of a class contains its package and class name.
+ * T/F The short name of a class is its package name.
+ * 
+ * The package name followed by . followed by the short name of the class is 
+ * its full or long name.
+ * The full name of this class is lectures.scanning.AConsoleReadingUpperCasePrinter 
+ * (if you have made a copy
  * it is lectures.scanning.copy.AConsoleReadingUpperCasePrinter).
  * 
  */
@@ -38,9 +57,11 @@ package lectures.scanning;
 	/**	  
 	 * 
 	 * IMPORTS
+	 * T/F It makes sense for a class to import only those classes that are 
+	 * not in its package.
 	 * 
-	 * If a class C1 refers a class C2 in a different (external) package, then it must use either the full name
-	 * of class C2 or import C2.
+	 * If a class C1 refers a class C2 in a different (external) package, 
+	 * then it must use either the full name of class C2 or import C2.
 	 * 
 	 * Below, we are importing an external class not in the package of this class.
 	 * What happens if you comment this import out?
@@ -52,8 +73,7 @@ package lectures.scanning;
 	 */
 import java.util.Scanner; 
 	/**
-	 * CLASSS
-	 * Every runnable program must have at least one class that declares a main method.
+	 * CLASSS	 
 	 * A class is like a file in non object oriented languages.
 	 * A class name should be camelCase starting with an upper case letter.
 	 * That is, the first word in the name should be a lowercase letter
@@ -62,24 +82,54 @@ import java.util.Scanner;
 public class AConsoleReadingUpperCasePrinter { 
 	
 	/**
-	 * MAIN METHOD HEADER
+	 * EXECUTABLE CLASSES and MAIN METHOD HEADER
+	 * T/F Only a class with a main method can be executed.  
+     * T/F A main method is the first method executed when its class is run.
+     * T/F A main method is the last method executed when its class is run.
+     * 
+	 * Every runnable program must have at least one class that declares a main method.
 	 * Syntax of main method shown below. This is the first method called when
-	 * the program is run, and can of course call othe methods.
+	 * the program is run, and can of course call other methods.
 	 * Methods correspond to procedures and functions in other languages.
 	 * A method name should be camelCase 
 	 * Everything before the first curly brace is the method header.
 	 * 
-	 * What is the purpose of the args parameter to the main method? (It is ok if you do not know the answer).
+	 * What is the purpose of the args parameter to the main method? 
+	 * (It is ok if you do not know the answer).
 	 */
 	public static void main(String[] args) 
-	/*
-	 * What happens if you use each of the following headers for main instead? Can you execute the program?
-	 * You cannot execute the program is you do not see the Java Application option in the Run As
-	 * or Debug As menu command, or if you see the console message, cannot find or load main method.
+	/* MAIN METHOD CONSTRAINTS
+	 * T/F A main method must have an argument named args.
+	 * T/F A main method can have a int argument.
+	 * T/F Constraints put on the header of the main method makes it easier for
+	 *  a human being to find it in the containing main class
+	 * T/F Constraints put on the header of the main main method make it easier
+	 *     for the Java interpreter, which executes a main method, to find it in the containing class.
+	 * T/F Constraints put on the header of the main method can prevent 
+	 *     programming errors such as returning a value that is not processed by the interpreter.
+	 * 
+	 * What happens if you use each of the following headers for main instead? 
+	 * Can you execute the program?
+	 * You cannot execute the program if you do not see the Java Application option 
+	 * in the Run As or Debug As menu command, or if you see the console message, 
+	 * cannot find or load main method.
+	 * 
 	 * Comment out the header above and uncomment the following to see what happens?
-	 * Does the program compile? Can the program be executed - do you get an option to run it?
-	 * You can comment and uncomment a set of selected lines using the shortcut CTRL/
-	 * In the case of a single line, it is sufficient to have the insertion point in the line.
+	 * Does the program compile? 
+	 * Can the program be executed - do you get an option to run it?
+	 * You can comment and uncomment a set of selected lines using the 
+	 * shortcut CTRL/
+	 * In the case of a single line, it is sufficient to have the insertion point 
+	 * in the line.
+	 * 
+	 * This exercise shows that a method with an arbitrary header cannot be
+	 * considered a main method. To understand why such constraints are necessary,
+	 * consider two entities, the Java interpreter and a human reading your code,
+	 * that must be able to search for the main method.
+	 * When you execute a class, the Java interpreter must find the method so
+	 * it can execute it and pass it the expected parameters.
+	 * When humans  reads your code, they must be able to determine the 
+	 * starting method without relying on perhaps missing or obsolete comments.
 	 * 	 
 	 */
 //	public static void main(String args) 
@@ -102,26 +152,41 @@ public class AConsoleReadingUpperCasePrinter {
 		 */
 		Scanner scanner = new Scanner(System.in);
 		/*
-		 * The next statement gives next line input by the user on the console, hover on method name to see its description
+		 * The next statement gives next line input by the user on the console, 
+		 * hover on method name to see its description
 		 */
 		String scannedString = scanner.nextLine(); 
 		
 		int index = 0; // variable names should be camel case starting with lower case letters 
 		/**
-		 * WHILE LOOPS
+		 * SCANNING AND WHILE LOOPS
+		 * (T/F) A token produced by scanning a string can have non consecutive 
+		 *       characters of the string.
+		 * (T/F) A string scanned by a scanner is a concatenation of all the tokens 
+		 * 		 detected by the scanner.
+		 * 
 		 * Syntax of Java while loop shown below
-		 * The loop "scans" scannedString, that is, it examines every character in the string and finds
+		 * The loop is  "scans" scannedString, that is, 
+		 * it examines every character in the string and finds
 		 * substrings of interest called tokens.
-		 * Characters are letters, digits, spaces and other values you can enter from the keyboard.
+		 * A substring SS of a string S is a sequence of consecutive characters
+		 * in S.
+		 * Not every character of a scanned string S is of interest, and thus,
+		 * part of an output token.
+		 * 	
+		 * Characters are letters, digits, spaces and other values you can enter 
+		 * from the keyboard.
 		 * A string can be considered an array or sequence of characters.
 		 * 
 		 * How many characters in each token identified by this loop?
-		 * Is this use of the term "scanning" consistent with its use in the English language?
+		 * Is this use of the term "scanning" consistent with its use in the 
+		 * English language?
 		 */
 		while (index < scannedString.length()) {
 			
 			/* 
-			 * charAt() gives character at index, hover over its name to get its behavior.
+			 * charAt() gives character at index, hover over its name to get its 
+			 * behavior.
 			 * A char is a type defined to store characters.
 			 */
 			char nextLetter = scannedString.charAt(index);
@@ -131,8 +196,10 @@ public class AConsoleReadingUpperCasePrinter {
 			 * Syntax of if statement with optional else shown below
 			 * the if check shows that characters are ordered
 			 * This ordering  preserves letter and digit ordering
-			 * You should only compare digits, lowercase letters and upper case letters with each other
-			 * not digits and letters or lower case and upper case with each other as these orderings are not fundamental
+			 * You should only compare digits, lowercase letters and upper case 
+			 * letters with each other
+			 * not digits and letters or lower case and upper case with each other 
+			 * as these orderings are not fundamental
 			*/
 			if (nextLetter >= 'A' && nextLetter <= 'Z') {
 				System.out.println("Token:" + nextLetter); // + here means string concatenation
--- src/lectures/scanning/package-info.java
+++ src/lectures/scanning/package-info.java
@@ -1,22 +1,28 @@
 /**
  * PRAXIS INFO
- * Code associated with the scanning teaching unit in: http://www.cs.unc.edu/~dewan/comp401/current/
- * The word and PPT documents created for the unit describe the purpose and motivation for this unit in
- * great depth.
+ * Code associated with the scanning teaching unit in: 
+ * http://www.cs.unc.edu/~dewan/comp401/current/
+ * The word and PPT documents created for the unit describe the purpose and 
+ * motivation for this unit in great depth. They form an alternative mechanism
+ * to answer at least some of the Sakai questions.
  * 
- * Copy this package for your praxis.
  * Visit the following classes for this praxis in this order:
- * {@link AConsoleReadingUpperCasePrinter} {@link AModularConsoleReadingUpperCasePrinter}
+ * {@link AConsoleReadingUpperCasePrinter} 
+ * {@link AModularConsoleReadingUpperCasePrinter}
  * 
  * 
- * Now click on the name of each referenced class name and Press F3 or CTRL-click (maybe Fn F3 or CMD F3, depending on your computer)
+ * Now click on the name of each referenced class name and Press F3 or CTRL-click 
+ * (maybe Fn F3 or CMD F3, depending on your computer)
  * to automatically navigate to it. 
  * 
  * 
- * After performing the praxis, answer the following questions in draft form here.
- * Once you are sure of your answers, take the Scanning Sakai quiz.
- * The praxis will help you answer not only these questions but also participate
- * in the class discussion. So even if you can answer the questions, do the praxis.
+ * The following questions are, in the scanning Sakai quiz, should be answerable
+ * after doing the exercise. You can write answers in draft form here,
+ * or directly answer the questions in Sakai.
+ * You may display the quiz and Eclipse window side by side. We have made
+ * an attempt to keep the length of these commented lines small (80 chars).
+ * 
+ * The 
  * 
  * SAKAI QUESTION INFO
  * 
@@ -36,22 +42,26 @@
  * 
  * Executable classes
  *
- * T/F Only a class with a main method can be executed.
- * 
+ * T/F Only a class with a main method can be executed.  
  * T/F A main method is the first method executed when its class is run.
  * T/F A main method is the last method executed when its class is run.
  * 
  * T/F A main method must have an argument named args.
  * T/F A main method can have a int argument.
- * T/F Constraints put on the header of the main method makes it easier for a human being to find it in the containing main class
- * T/F Constraints put on the header of the main main method make it easier for the Java interpreter, which executes a main method, to find it in the containing class.
- * T/F Constraints put on the header of the main method can prevent programming errors such as returning a value that is not processed by the interpreter.
+ * T/F Constraints put on the header of the main method makes it easier for a 
+ *     human being to find it in the containing main class
+ * T/F Constraints put on the header of the main main method make it easier for 
+ *     the Java interpreter, which executes a main method, to find it in the containing class.
+ * T/F Constraints put on the header of the main method can prevent programming
+ *     errors such as returning a value that is not processed by the interpreter.
  * 
  * 
  * Scanning
  * 
- * (T/F) A token produced by scanning a string can have non consecutive characters of the string.
- * (T/F) A string scanned by a scanner is a concatenation of all the tokens detected by the scanner.
+ * (T/F) A token produced by scanning a string can have non consecutive 
+ *       characters of the string.
+ * (T/F) A string scanned by a scanner is a concatenation of all the tokens 
+ * 		 detected by the scanner.
  *
  * DISCUSSION INFO
  * What did you learn or re-learn in through this praxis and the questions?
