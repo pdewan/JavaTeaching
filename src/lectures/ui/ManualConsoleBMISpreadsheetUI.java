bebcd67c22f0bf950072f0007b6c15bb1fd8e8eb
pdewan <dewan@cs.unc.edu>
2017-09-17 14:36:27 -0400

    Removed spurious file. Improved UI praxis.

--- src/lectures/ui/ManualConsoleBMISpreadsheetUI.java
+++ src/lectures/ui/ManualConsoleBMISpreadsheetUI.java
@@ -12,57 +12,11 @@ import java.util.Scanner;
  * between (programmed) objects.
  * 
  * Another kind of interfaces - user interfaces - allow users to interact with 
- * (programmed)objects.
+ * (programmed)objects. 
  * 
  * They are interfaces between (programmed) objects and users. 
  * 
- * User interfaces have to be implemented by user-interface code. They are
- * not automatically provided by Java, though there are libraries to
- * make them easy to write.
- * 
- * (T/F) A programming interface of an object allows another object to
- * interact with it. 
- * 
- * (T/F) A user interface of an object allows a human user to
- * interact with it.
- * 
- * (T/F) Java automatically provides a user-interface to interact with each object.
- * 
- * 
- * EXTENT OF USER-INTERFACE CODE
- * 
- * All instances of a class, of course, share the same user-interface code just
- * as they share the same (programming) interface declaration.
- * 
- * Creating an interface definition is relatively trivial (once we understand the concept).
- * Creating user-interface code is a more laborious job. 
- * 
- * The size of the user-interface code we write to interface with a class of objects
- * is usually proportional to the size of the class. 
- * 
- * The class for which the UI is created is called the computation or model class 
- * and the code in it is called the model or computation code.
- * 
- * This class provides code for implementing the user-interface of  AUIBMISpreadsheet,
- * which is an extension of ABMISpreadsheet that has two additional methods for
- * incrementing the height and weight to do do what-if computations.
- * 
- * Run this class to see what it does and terminate it by pressing on the red button to kill
- * the program.
- * 
- * 
- * Go to AUIBMISpreadsheet and count the number of lines in that class.
- * 
- * Now count the number of lines of code in this class. Exclude comments if you can.
- * 
- * To show line numbers, Go to Windows -> Preferences->General ->Text Editors ->
- * Show line numbers.
- * 
- *  Based on the ManualConsoleBMISpreadSheetUI example, the UI code is:
- * 	   (a) orders of magnitude (6 or more times) smaller than the computation code.
- * 	   (b) orders of magnitude (6 or more times)  larger than the computation code.
- *     (c) about the same size as the computation code (none of the above).  
- * 
+ * This class illustrates user-interfaces. 
  */
 public class ManualConsoleBMISpreadsheetUI {
 	static Scanner scanner = new Scanner(System.in);
@@ -86,7 +40,88 @@ public class ManualConsoleBMISpreadsheetUI {
 		}		
 	}
 /*
-
+ * A TALE OF TWO INTERFACES  
+ *
+ * 
+ * User interfaces have to be implemented by user-interface code. They are
+ * not automatically provided by Java, though there are libraries to
+ * make them easy to write.
+ * 
+ * (T/F) A programming interface of an object allows another object to
+ * interact with it. 
+ * 
+ * (T/F) A user interface of an object allows a human user to
+ * interact with it.
+ * 
+ * (T/F) Java automatically provides a user-interface to interact with each object.
+ * 
+ */
+/*
+ * EXTENT OF USER-INTERFACE CODE
+ * 
+ * All instances of a class, of course, share the same user-interface code just
+ * as they share the same (programming) interface declaration.
+ * 
+ * Creating an interface definition is relatively trivial (once we understand the concept).
+ * Creating user-interface code is a more laborious job. 
+ * 
+ * The size of the user-interface code we write to interface with a class of objects
+ * is usually proportional to the size of the class. 
+ * 
+ * The class for which the UI is created is called the computation or model class 
+ * and the code in it is called the model or computation code. Such
+ * code does not directly do any input or output. The UI code is 
+ * responsible for input and/or output.
+ * 
+ * It processes input by converting user input
+ * (such as entering text in a console or a GUI) and converts it into
+ * a Java value used by computation code. 
+ * 
+ * It processes output by converting some value computed by the 
+ * model code  into output that can be viewed by the user.
+ * 
+ * User-interface code is responsible for:
+ *    (a) converting user input into Java values.
+ *    (b) converting Java values into user output.
+ *    (c) determining the relationship between output and input. 
+ *  
+ *  Computation code is responsible for:
+ *    (a) converting user input into Java values.
+ *    (b) converting Java values into user output.
+ *    (c) determining the relationship between output and input.   
+ * 
+ * We know this main class is a UI class and it does nothing but 
+ * read from the console and output to the console, 
+ * letting the computation of
+ * BMI be done by ABMISpreadsheetComputationClass.
+ * 
+ * Look at the code of this class to verify it 
+ * does UI and not computation.
+ * 
+ * Run this main class to see what it does and terminate it by
+ * pressing on the red button to kill the program.
+ * 
+ * To make our programming easier, this class does not provide access to the 
+ * full functionality of ABMISpreadsheetComputationClass: It does not provide
+ * access to the increment operations nor does it do error checking.
+ * 
+ * Run this class to see what it does and terminate it by pressing on the red button to kill
+ * the program.
+ *   
+ * Go to AUIBMISpreadsheet and count the number of lines in that class.
+ * 
+ * Now count the number of lines of code in this class. Exclude comments if you can.
+ * 
+ * To show line numbers, Go to Windows -> Preferences->General ->Text Editors ->
+ * Show line numbers.
+ * 
+ *  Based on the ManualConsoleBMISpreadSheetUI example, the UI code is:
+ * 	   (a) orders of magnitude (6 or more times) smaller than the computation code.
+ * 	   (b) orders of magnitude (6 or more times)  larger than the computation code.
+ *     (c) about the same size as the computation code (none of the above).  
+ * 
+ */
+/*
  * How can we reduce the problem of writing UI code? 
  * 
  * Eclipse will automatically extract a programming interface from a class.
