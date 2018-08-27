547e5333850244f0800a7056145c4e2c620fbef9
pdewan <dewan@cs.unc.edu>
2018-08-27 16:49:38 -0400

    Added clarifications to package-info

--- src/lectures/class_dual_roles/statics/package-info.java
+++ src/lectures/class_dual_roles/statics/package-info.java
@@ -2,8 +2,16 @@
 /**
  * We now see ways and motivation for creating a program with multiple classes.
  * While only one class will be runnable - have a main method - there will be
- * many supporting classes, and we will see a principle for decomposing
- * code into multiple classes and another for protecting the variables and methods in
+ * many supporting classes. As before the questions will ask your to change 
+ * something in the code and 'see what happens'. Your  first instinct to see 
+ * what happens is to try and run the class. However, many of these classes 
+ * do not have a 'main' method (so they cannot be run). This implies that your  
+ * are simply supposed to change the class and see the kind of compile errors 
+ * shown in the Eclipse editor as opposed to running something to 
+ * examine the output and find any errors in it. 
+ * 
+ * We see here a principle for decomposing code into multiple classes
+ * and another for protecting the variables and methods in
  * them from external scrutiny. While decomposition has many benefits, it has
  * the disadvantage that the code is distributed. This means the program becomes
  * like the elephant in the story of the blind men and an elephant 
