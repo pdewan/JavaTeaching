d881ff758ae19339b142ddab4e6574e87cfa3563
pdewan <dewan@cs.unc.edu>
2018-08-27 18:03:31 -0400

    Added some question text to clariifications

--- src/lectures/class_dual_roles/instances/Clarifications.java
+++ src/lectures/class_dual_roles/instances/Clarifications.java
@@ -8,15 +8,30 @@ During office hours with Professor Dewan, I had asked about the following dozen 
 Question 27:
 The phrasing "set of values (called Instances) of that types" really confused me. It helps to think of this in terms of memory allocation. If one were to assign an instanced class to a variable, and then create a new instance, would the amount of memory allocated change? 
 Question 34-37:
+An instance method can be invoked on a class, that is, it can be invoked by prefixing the method name with the name of the class + ".'., i.e using the class.method format.
 The phrasing at the end of these questions through me off. It means "class.method" format, play with the praxis to test these out.
+Each read method of ALoopingFactorialSpreadsheet accesses (refers to) a different set of instance variables.
 Question 40 and 44:
 This is similar to question 27, in how to think about a set of values. Dr.Prasun told me that the answers to these two may be incorrect and that he'd explain them in class
+Each read method of ALoopingFactorialSpreadsheet accesses (refers to) a different set of instance variables.
+Each read method of AnotherLoopingFactorialSpreadsheet accesses a different set of instance variables.
 Question 46 and 49: 
 Public behavior means the results of public methods. Compare the set of public methods of each class and the outputs of the public methods in each set.
+AnotherLoopingFactorialSpreadsheet and ALoopingFactorialSpreadsheet have the same public behavior, that is behavior defined by the execution of public methods.
 Question 48, 50, 51, 53, and 55: 
+For each variable of AFactorialSpreadsheetPretender there exists a variable with the same name and type in 
+ALoopingFactorialSpreadsheet.
+For each property of AnotherFactorialSpreadsheetPretender there exists a property with the same name and type in
+ALoopingFactorialSpreadsheet.
+For each variable of AnotherFactorialSpreadsheetPretender there exists a variable with the same name and type in ALoopingFactorialSpreadsheet.
+For each property of ANonBeanLoopingFactorialSpreadsheet there 
+exists a property with the same name and type in ALoopingFactorialSpreadsheet.
+For each property of ALoopingFactorialSpreadsheetWithNonPublicMethods there exists a property with the same name and type in 
+ALoopingFactorialSpreadsheet.
 The formula of these questions is incorrect. They're supposed to read for each value/property in A there is a value/property in B. Flip A and B where A and B are both classes.
 I believe these questions will not be graded (worth 0 points)
 Question 64:
+The names and types of the properties defined by a class depend on
 This question is a bit tricky to read. Consider what a property is, how they're declared, and how they're used. The protections (private/public) of properties may also help you understand what affects them.
 In general, I have found three solutions to solving a question I have genuinely no clue about, in order of time efficiency:
 Office hours/asking
