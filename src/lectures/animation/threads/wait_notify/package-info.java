fe8aaaf3185f2c72bbfad9fce549546156c7c1d0
dewan <dewan@cs.unc.edu>
2019-12-08 18:58:48 -0500

    Pckage-info file

---
+++ src/lectures/animation/threads/wait_notify/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * PRAXIS-INFO
+ * This praxis illustrates both wait and notify.
+ * 
+ * A lock is implemented using wait and notify.
+ * 
+ * NAVIGATION-INFO
+ * ManualShuttleTrafficControl
+
+ * 
+ */
+
+package lectures.animation.threads.wait_notify;
